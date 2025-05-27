763e86e6e0493fe2395492a59ba7fae5923df52b
Kanika0503 <99464750+Kanika0503@users.noreply.github.com>
2025-05-27 15:24:53 +0530

    Update fuzzy_engine.py

--- fuzzy_engine.py
+++ fuzzy_engine.py
@@ -2,43 +2,41 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-def evaluate_node(cpu_val, mem_val, net_val, latency_val, disk_val, temp_val, threat_val):
-    cpu = ctrl.Antecedent(np.arange(0, 101, 1), 'cpu')
-    mem = ctrl.Antecedent(np.arange(0, 101, 1), 'mem')
-    net = ctrl.Antecedent(np.arange(0, 101, 1), 'net')
-    latency = ctrl.Antecedent(np.arange(0, 101, 1), 'latency')
-    disk = ctrl.Antecedent(np.arange(0, 101, 1), 'disk')
-    temp = ctrl.Antecedent(np.arange(0, 101, 1), 'temp')
-    threat = ctrl.Antecedent(np.arange(0, 11, 1), 'threat')
-    score = ctrl.Consequent(np.arange(0, 101, 1), 'score')
+# Define fuzzy variables
+cpu = ctrl.Antecedent(np.arange(0, 101, 1), 'cpu')
+mem = ctrl.Antecedent(np.arange(0, 101, 1), 'mem')
+latency = ctrl.Antecedent(np.arange(0, 101, 1), 'latency')
+threat = ctrl.Antecedent(np.arange(0, 11, 1), 'threat')
+score = ctrl.Consequent(np.arange(0, 101, 1), 'score')
 
-    for ant in [cpu, mem, net, latency, disk, temp]:
-        ant.automf(3)
+# Membership functions
+cpu.automf(3)
+mem.automf(3)
+latency.automf(3)
+threat['low'] = fuzz.trimf(threat.universe, [0, 0, 5])
+threat['medium'] = fuzz.trimf(threat.universe, [2, 5, 8])
+threat['high'] = fuzz.trimf(threat.universe, [6, 10, 10])
 
-    threat['low'] = fuzz.trimf(threat.universe, [0, 0, 5])
-    threat['medium'] = fuzz.trimf(threat.universe, [0, 5, 10])
-    threat['high'] = fuzz.trimf(threat.universe, [5, 10, 10])
+score['low'] = fuzz.trimf(score.universe, [0, 0, 50])
+score['medium'] = fuzz.trimf(score.universe, [25, 50, 75])
+score['high'] = fuzz.trimf(score.universe, [50, 100, 100])
 
-    score['poor'] = fuzz.trimf(score.universe, [0, 0, 50])
-    score['average'] = fuzz.trimf(score.universe, [25, 50, 75])
-    score['good'] = fuzz.trimf(score.universe, [50, 100, 100])
+# Fuzzy rules
+rules = [
+    ctrl.Rule(cpu['good'] & mem['good'] & latency['poor'] & threat['low'], score['high']),
+    ctrl.Rule(cpu['average'] | mem['average'] | latency['average'] | threat['medium'], score['medium']),
+    ctrl.Rule(cpu['poor'] | mem['poor'] | latency['good'] | threat['high'], score['low']),
+]
 
-    rules = [
-        ctrl.Rule(cpu['good'] & mem['good'] & threat['low'], score['good']),
-        ctrl.Rule(cpu['average'] | mem['average'] | threat['medium'], score['average']),
-        ctrl.Rule(cpu['poor'] | mem['poor'] | threat['high'], score['poor']),
-    ]
+scoring_ctrl = ctrl.ControlSystem(rules)
+scoring_sim = ctrl.ControlSystemSimulation(scoring_ctrl)
 
-    system = ctrl.ControlSystem(rules)
-    sim = ctrl.ControlSystemSimulation(system)
-
+# Evaluation function
+def evaluate_node(cpu_val, mem_val, latency_val, threat_val):
+    sim = ctrl.ControlSystemSimulation(scoring_ctrl)
     sim.input['cpu'] = cpu_val
     sim.input['mem'] = mem_val
-    sim.input['net'] = net_val
     sim.input['latency'] = latency_val
-    sim.input['disk'] = disk_val
-    sim.input['temp'] = temp_val
     sim.input['threat'] = threat_val
-
     sim.compute()
     return sim.output['score']
